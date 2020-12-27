4956609f4d31250a09cc78115e516b1d7d46f49b
Ritik <nandanwarritik10@gmail.com>
2020-12-27 11:05:53 +0530

    Connected socket.io

--- src/Component/Chat.js
+++ src/Component/Chat.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import queryString from "query-string"
+import io  from "socket.io-client"
 
-const Chat = () => {
+let socket
+var connectionOptions =  {
+  "force new connection" : true,
+  "reconnectionAttempts": "Infinity", 
+  "timeout" : 10000,                  
+  "transports" : ["websocket"]
+};
+
+const Chat = ({location}) => {
+  const [name , setName] = useState("")
+  const [room , setRoom] = useState("")
+
+  useEffect(() => {
+    const {name , room} = queryString.parse(location.search)
+    socket = io("localhost:3333" , connectionOptions)
+    setName(name)
+    setRoom(room)
+    console.log(socket)
+  },[])
   return (
     <div>
       Chat
--- src/Component/Join.js
+++ src/Component/Join.js
@@ -4,21 +4,19 @@ import { Link } from 'react-router-dom'
 const Join = () => {
   const [name , setName] = useState("")
   const [room , setRoom] = useState("")
-  const onInputChange = (e) => {
-    console.log(e.target.value)
-  }
+  
   return (
     <div>
       <div className="container grey lighten-3" style={{justifyContent:"center" , margin: "30% auto" , padding:"5rem"}}>
         <form action="">
           <h4 className="green-text">join room</h4>
-          <input onChange={ (e) => {
-            onInputChange(e)
-          } } required type="text" className="input-field"placeholder="Enter your name"/>
-          <input onChange={ (e) => {
-            onInputChange(e)
-          } } required type="text" className="input-field" placeholder="Enter room name"/>
-          <Link type="submit" className="btn blue white-text right" to="/chat">Join</Link>
+          <input onChange={(e) => {
+            setName(e.target.value)
+          }} required type="text" className="input-field"placeholder="Enter your name"/>
+          <input onChange={(e) => {
+            setRoom(e.target.value)
+          }} required type="text" className="input-field" placeholder="Enter room name"/>
+          <Link type="submit" className="btn blue white-text right" to={`/chat?name=${name}&room=${room}`}>Join</Link>
         </form>
         
       </div>
